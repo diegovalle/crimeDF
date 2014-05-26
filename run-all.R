100aee98004f76d0307bb777405d9b997a769f2d
Diego Valle-Jones <diegovalle@gmail.com>
2014-05-25 21:31:45 -0500

    create json and graphs

--- run-all.R
+++ run-all.R
@@ -2,4 +2,5 @@
 source(file.path("src", "packages.R"))
 source(file.path("src", "clean.R"))
 source(file.path("src", "map-clean.R"))
-#source(file.path("src", "graphs.R"))
+source(file.path("src", "graphs.R"))
+source(file.path("src", "json.R"))
